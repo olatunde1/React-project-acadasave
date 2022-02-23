594c8b139a0c7512bec54826e29163caea53c322
Olatunde <idowuolatunde32@gmail.com>
2022-02-23 16:53:58 +0100

    first commit

---
+++ src/component/How.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const How = () => {
+  return (
+    <div className='container'>
+        <div class="row ">
+            <h4>How it Works</h4>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default How
---
+++ src/component/Navigation.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Logo from "../images/logo.png"
+import "./navigation.css"
+
+const Navigation = () => {
+  return (
+    <div className='container'>
+    
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"><img src={Logo} alt="" /></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav m-auto">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">How it works</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Packages</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Benefits</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <button className="login" type="Log-in">Log in</button>
+              <button className="signUp" type="Sign-up">Sign up</button>
+              
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navigation
