bf90d25779f79a887096349f8c18ba2d9915b170
Olatunde <idowuolatunde32@gmail.com>
2022-02-24 14:49:59 +0100

    update on the cards

--- src/component/How.jsx
+++ src/component/How.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./how.css"
 import img1 from "../images/img1.png"
 import img22 from "../images/img22.png"
 import img33 from "../images/img33.png"
@@ -6,64 +7,66 @@ import img44 from "../images/img44.png"
 
 const How = () => {
   return (
-    <div className='container'>
-        <div class="row ">
-            <h4>How it Works</h4>
-            <div class="col-sm-3">
-                <div class="card">
-                <img src={img1} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                <h5 class="card-title">Register</h5><br/>
-            <p class="card-text">Lorem ipsum dolor sit amet 
-            consectetur adipisicing elit. 
-            Quaerat saepe voluptatem provident 
-            voluptatibus exercitationem facere natus tempore
-            dignissimos suscipit harum placeat repellendus totam sequi.</p>
+    <section className='how-it-works'>
+        <div className='container'>
+            <div class="row ">
+                <h4>How it Works</h4>
+                <div class="col-sm-3">
+                    <div class="card border-0">
+                        <img src={img1} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Register</h5><br/>
+                            <p class="card-text">Lorem ipsum dolor sit amet 
+                            consectetur adipisicing elit. 
+                            Quaerat saepe voluptatem provident 
+                            voluptatibus exercitationem facere natus tempore
+                            dignissimos suscipit harum placeat repellendus totam sequi.</p>
+                        </div>
+                    </div>
                 </div>
+                <div class="col-sm-3">
+                    <div class="card border-0">
+                        <img src={img22} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Save</h5> <br />
+                            <p class="card-text">Lorem ipsum dolor sit amet 
+                            consectetur adipisicing elit. 
+                            Quaerat saepe voluptatem provident 
+                            voluptatibus exercitationem facere natus tempore
+                            dignissimos suscipit harum placeat repellendus totam sequi.</p>                
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-3">
-                <div class="card">
-                <img src={img22} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Save</h5> <br />
-            <p class="card-text">Lorem ipsum dolor sit amet 
-            consectetur adipisicing elit. 
-            Quaerat saepe voluptatem provident 
-            voluptatibus exercitationem facere natus tempore
-            dignissimos suscipit harum placeat repellendus totam sequi.</p>                
+                <div class="col-sm-3">
+                    <div class="card border-0">
+                        <img src={img33} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Get Interest</h5><br />
+                            <p class="card-text">Lorem ipsum dolor sit amet 
+                            consectetur adipisicing elit. 
+                            Quaerat saepe voluptatem provident 
+                            voluptatibus exercitationem facere natus tempore
+                            dignissimos suscipit harum placeat repellendus totam sequi.</p>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-            <div class="col-sm-3">
-                <div class="card">
-                <img src={img33} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Get Interest</h5><br />
-            <p class="card-text">Lorem ipsum dolor sit amet 
-            consectetur adipisicing elit. 
-            Quaerat saepe voluptatem provident 
-            voluptatibus exercitationem facere natus tempore
-            dignissimos suscipit harum placeat repellendus totam sequi.</p>
-                </div>
-                </div>
-            </div>
-            <div class="col-sm-3">
-                <div class="card">
-                <img src={img44} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Withdraw</h5>
-                    <br />
-            <p class="card-text">Lorem ipsum dolor sit amet 
-            consectetur adipisicing elit. 
-            Quaerat saepe voluptatem provident 
-            voluptatibus exercitationem facere natus tempore
-            dignissimos suscipit harum placeat repellendus totam sequi.</p>
-                </div>
+                <div class="col-sm-3">
+                    <div class="card border-0">
+                        <img src={img44} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">Withdraw</h5>
+                            <br />
+                            <p class="card-text">Lorem ipsum dolor sit amet 
+                            consectetur adipisicing elit. 
+                            Quaerat saepe voluptatem provident 
+                            voluptatibus exercitationem facere natus tempore
+                            dignissimos suscipit harum placeat repellendus totam sequi.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
   )
 }
 
