f37af3481da42feeae75de8d0126e56bf6197e81
Olatunde <idowuolatunde32@gmail.com>
2022-02-27 12:59:46 +0100

    footer responsiveness completed

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,38 +5,50 @@ import white from "../images/white.png"
 
 const Footer = () => {
   return (
+    
     <footer className="footerEnd">
-      <div className="container">
-            <div className="card-footer">
+      <div className="getStart">
                 <div className="row">
-                      <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 ">
-                          <img src={white} alt="" />
-                          <p className='lorem'>Lorem ipsum dolor sit, amet consectetur <br />
-                          adipisicing elit.Magni sequi, minima ut possimus <br />
-                          repellendus, odio ratione quo sit autem, exercitationem <br />
-                          in distinctio. <br />
-                          </p>
-                      </div>
-                      <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 ">
-                          <h5>Quick Links</h5>
-                          <p className='aboutUs'>About us</p>
-                      </div>
-                </div>
-                    <div className="getStart">
-                      <div className="row">
-                          <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-                                <h3>Ready to get Started</h3> 
-                                <p>Create new account and start saving.</p>
-                          </div>
+                    <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                          <h3>Ready to get Started</h3> 
+                          <p>Create new account and start saving.</p>
+                    </div>
 
-                          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center ">
-                                <button type='btn'>GET STARTED</button>
-                          </div>
-                                            
-                      </div>
+                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center ">
+                          <button type='btn'>GET STARTED</button>
+                    </div>
+                                      
                 </div>
-            </div> 
           </div>
+        <div className="footer-content">
+        
+      <div className="container">
+        <div className="card-footer">
+        
+          <div className="div">
+          <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-10 col-lg-10 ">
+                    <img src={white} alt="" />
+                    <p className='lorem'>Lorem ipsum dolor sit, amet consectetur <br />
+                    adipisicing elit.Magni sequi, minima ut possimus <br />
+                    repellendus, odio ratione quo sit autem, exercitationem <br />
+                    in distinctio. <br />
+                    </p>
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-2 col-lg-2 ">
+                  <div className="quicklinks">
+                    <h5>Quick Links</h5>
+                    <p className='aboutUs'>About us</p>
+                  </div>
+               
+                </div>
+          </div>
+          </div>
+        </div> 
+      </div>
+        </div>
+      
+      
     </footer>
             
     
--- src/component/LoanAccess.jsx
+++ src/component/LoanAccess.jsx
@@ -8,7 +8,7 @@ const LoanAccess = () => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,192L120,208C240,224,480,256,720,234.7C960,213,1200,139,1320,101.3L1440,64L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
         <div className="container">
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                         <div className="loan-caption">
                             <h2>Access to loan for various <br/> purposes</h2>
                                 <p><div className="question">Why choose us</div></p>
@@ -19,7 +19,7 @@ const LoanAccess = () => {
                             <input className="LoanButton" type="submit" value="GET STARTED"/>
                         </div>
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                         <div className="loan-img">
                         <img src={img55} alt=""/>
                         </div>
