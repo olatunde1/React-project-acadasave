38272153dd2e693b270f772c01476fbd774c3205
Olatunde <idowuolatunde32@gmail.com>
2022-02-28 22:25:43 +0100

    Testimony session completed

--- src/component/Packages.jsx
+++ src/component/Packages.jsx
@@ -58,6 +58,11 @@ const Packages = () => {
         
       </div>
     </div>
+    <div className='svgDiv'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#f3f4f5" fill-opacity="1" d="M0,0L80,32C160,64,320,128,480,138.7C640,149,800,107,960,85.3C1120,64,1280,64,1360,64L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+      </svg>
+    </div>
     </section>
     
               
--- src/component/Testimony.jsx
+++ src/component/Testimony.jsx
@@ -1,33 +1,46 @@
 import React from 'react'
 import "./testimony.css"
 import quote1 from "../images/quote.png"
+import img3 from "../images/img3.png"
+import img4 from "../images/img4.jpg"
+import img13 from "../images/img13.jpg"
 
 const Testimony = () => {
   return (
-      <section className='testimony-section'>
-          <div className='container'>
-
-                <div className="row">
-                    <div className='svgDiv'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                            <path fill="#f3f4f5" fill-opacity="1" d="M0,0L80,32C160,64,320,128,480,138.7C640,149,800,107,960,85.3C1120,64,1280,64,1360,64L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
-                        </svg>
+    <section>
+        <div className='testimony'>
+            <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div className="cusFeed">
+                        <img src={quote1} alt="" />
+                            <h4>What Our <br /> Customers are <br />Saying</h4>
+                            <i className="fa fa-arrow-left arrowLeft" ></i>
+                            <i className="fa fa-arrow-right arrowRight" ></i>
                     </div>
 
-                  <div className="col-sm-4">
-                    <img src={quote1} alt="" />
-                    <h4>What Our <br /> Customers are <br />Saying</h4>
-                  </div>
-                  <div className="col-sm-8">
-                      
-                  </div>
-                 
-              </div>
-            
-          </div>
-         
-      </section>
-    
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                   
+                    <div className="row ">
+                        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                            <div className='testify'>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus ipsa voluptatum ullam iure. Eligendi expedita dignissimos quibusdam, suscipit aliquam ea vero earum ducimus consequatur quisquam inventore, corrupti fugiat quia ipsa.</p>
+                                <hr />
+                                <p>Samuel Tola</p>
+                            </div>
+                        </div>
+                        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                            <div className='testify'>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus ipsa voluptatum ullam iure. Eligendi expedita dignissimos quibusdam, suscipit aliquam ea vero earum ducimus consequatur quisquam inventore, corrupti fugiat quia ipsa.</p>
+                                <hr />
+                                <p>Kemi Williams</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>                       
+            </div>
+        </div>
+    </section>
   )
 }
 
